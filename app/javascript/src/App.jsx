162a176c0363f3d9950d5a6c2777a7a70671c9db
Alfred Nelson <alfrednelson375@gmail.com>
2021-11-29 15:36:14 +0530

    Added linting and formating code

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div className="w-full flex justify-center m-10">Home</div>} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="w-full flex justify-center m-10">Home</div>
+          )}
+        />
       </Switch>
     </Router>
   );
