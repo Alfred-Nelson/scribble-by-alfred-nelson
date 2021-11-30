25e1daa5cf1bbd02c133c118cb28f675659f7da2
Alfred Nelson <alfrednelson375@gmail.com>
2021-11-30 07:56:03 +0530

    Added user model and added necessary axios header

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import { setAuthHeaders, registerIntercepts } from "apis/axios";
+import Dashboard from "components/Dashboard";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setAuthHeaders(setLoading);
+    registerIntercepts();
+  }, []);
+
+  if (loading) {
+    return <p>Loading ... </p>;
+  }
+
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <div className="w-full flex justify-center m-10">Home</div>
-          )}
-        />
+        <Route exact path="/" render={Dashboard} />
       </Switch>
     </Router>
   );
